6c463dde9b8e603abfb1acb2ca4add9ea24949c0
SANTANU GIRI <124768719+girisantanu@users.noreply.github.com>
2023-09-27 19:29:09 +0530

    Create lab3 q2 maximum and the minimum value.R

---
+++ lab3 q2 maximum and the minimum value.R
@@ -0,0 +1,9 @@
+# Write a R program to find the maximum and the minimum value of a given vector
+
+given_vector <- c(23, 45, -12, 6, 0, -35, 18, 42, -8, 15)
+
+max_value <- max(given_vector)
+min_value <- min(given_vector)
+
+cat("Maximum value:", max_value, "\n")
+cat("Minimum value:", min_value, "\n")
