61bf6a91497d28ace9a00398cbe10592c2f7f3b7
SANTANU GIRI <124768719+girisantanu@users.noreply.github.com>
2023-09-27 19:30:41 +0530

    Create lab3 q3  create three vectors numeric data, character data and logical data.R

---
+++ lab3 q3  create three vectors numeric data, character data and logical data.R
@@ -0,0 +1,11 @@
+#3. Write a R program to create three vectors numeric data, character data and logical data. Display the content of the vectors and their type.
+
+numeric_vector <- c(11,32,45,78)
+charecter_vector <- c("red","yellow","blue","green")
+logical_vector <- c(T,F,T,F)
+cat("numeric vector:\n")
+print(numeric_vector)
+cat("charecter vector:\n")
+print(charecter_vector)
+cat("logical vector:\n")
+print(logical_vector)
